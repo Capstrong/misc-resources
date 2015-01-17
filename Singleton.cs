eb3f7c952eedd5de218567efa890b68f3e8216af
Zac <brickman1444@gmail.com>
2015-01-16 22:02:41 -0600

    Adding singleton script from Train.

---
+++ Singleton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+// Make a subclass of this class with T as the subclass to make a singleton
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance = null;
+
+    public static T instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.name = typeof(T).Name;
+                    _instance = go.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    public static bool DoesExist()
+    {
+        return _instance != null;
+    }
+
+    // Call this to upgrade a singleton to a persistent singleton.
+    // This is most often done in Awake().
+    // This will kill an instance that tries to be a persistent singleton but isn't the current instance.
+    public void DontDestroyElseKill(MonoBehaviour mb)
+    {
+        if (mb == instance)
+        {
+            MonoBehaviour.DontDestroyOnLoad(instance.gameObject);
+        }
+        else
+        {
+            MonoBehaviour.Destroy(mb);
+        }
+    }
+}
